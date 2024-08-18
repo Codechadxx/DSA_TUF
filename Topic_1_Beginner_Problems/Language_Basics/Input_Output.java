dc3cb4458746bd45a42c30986e6c69838520a277
Rachit kumar <154961524+Codechadxx@users.noreply.github.com>
2024-08-18 14:25:20 +0530

    DSA

---
+++ Topic_1_Beginner_Problems/Language_Basics/Input_Output.java
@@ -0,0 +1,11 @@
+package DSA.Topic_1_Beginner_Problems.Language_Basics;
+
+import java.util.Scanner;
+
+public class Input_Output {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+    }
+}
